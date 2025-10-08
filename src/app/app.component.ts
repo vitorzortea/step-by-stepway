d2e5eaa053e02c7dc2805f1d4e5d28c8c9745fca
Vitor Zortéa <vitorzortea@gmail.com>
2025-10-08 14:43:34 -0300

    Subir o Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'step-by-stepway';
+
+    private http = inject(HttpClient);
+    test:Observable<{total:number,entrada:number,parcelas:any[]}>;
+
+    constructor(){
+      this.test = this.http
+      .get<{total:number,entrada:number,parcelas:any[]}>('json/bd.json');
+    }
+    parcelas(parcelas:any[]){
+      return parcelas.filter(e=>e.pago).reduce((a,b)=>a+b.valor, 0);
+    }
+    aPagar(dados:any){
+      return dados.total - (dados.entrada+this.parcelas(dados.parcelas));
+    }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+  ]
 };
